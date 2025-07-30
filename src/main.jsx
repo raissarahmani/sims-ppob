2ac0f0c21aded48d40399b2107a31d3d28c3d7f7
rrhmn <raissarahmani18@gmail.com>
2025-07-30 17:37:35 +0700

    init setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter } from 'react-router'
+import './styles/tailwind.css'
+import Router from './Router'
+import ReduxProvider from './redux/ReduxProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ReduxProvider>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ReduxProvider>
   </StrictMode>,
 )
