f49944d0efeeaa31599fa1939c003b5e8803d43b
rrhmn <raissarahmani18@gmail.com>
2025-07-31 22:49:06 +0700

    fix: mock up data moved to data folder

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,18 +3,14 @@ import { useState, useRef } from 'react'
 import Service from './Service'
 import Banner from './Banner'
 
-import Game from '../../assets/Game.png'
-import Kurban from '../../assets/Kurban.png'
-import Listrik from '../../assets/Listrik.png'
-import Musik from '../../assets/Musik.png'
-import PaketData from '../../assets/PaketData.png'
-import PBB from '../../assets/PBB.png'
-import PDAM from '../../assets/PDAM.png'
-import PGN from '../../assets/PGN.png'
-import Pulsa from '../../assets/Pulsa.png'
-import Televisi from '../../assets/Televisi.png'
-import Makanan from '../../assets/VoucherMakanan.png'
-import Zakat from '../../assets/Zakat.png'
+import ServiceList from '../../data/services.json'
+import BannerList from '../../data/banner.json'
+
+const images = import.meta.glob('../../assets/*.png', { eager: true })
+
+function getImage(filename) {
+  return images[`../../assets/${filename}`]?.default
+}
 
 function Home() {
   const containerRef = useRef(null);
@@ -39,34 +35,11 @@ function Home() {
   const onMouseUp = () => setIsDragging(false);
   const onMouseLeave = () => setIsDragging(false);
 
-  const services = [
-    { image: PBB, name: 'PBB' },
-    { image: Listrik, name: 'Listrik' },
-    { image: Pulsa, name: 'Pulsa' },
-    { image: PDAM, name: 'PDAM' },
-    { image: PGN, name: 'PGN' },
-    { image: Televisi, name: 'TV Langganan' },
-    { image: Musik, name: 'Musik' },
-    { image: Game, name: 'Voucher Game' },
-    { image: Makanan, name: 'Voucher Makanan' },
-    { image: Kurban, name: 'Kurban' },
-    { image: Zakat, name: 'Zakat' },
-    { image: PaketData, name: 'Paket Data' },
-  ]
-
-  const banners = [
-    { src: '/Banner1.png', alt:'Saldo Gratis'},
-    { src: '/Banner2.png', alt:'Diskon Listrik'},
-    { src: '/Banner3.png', alt:'Promo Makan'},
-    { src: '/Banner4.png', alt:'Cashback 25%'},
-    { src: '/Banner5.png', alt:'Buy 1 Get 2'},
-  ]
-
   return (
     <div className='px-30 py-5'>
       <div className='flex flex-row justify-between items-center my-5'>
-        {services.map((service, i) => (
-          <Service key={i} image={service.image} name={service.name} />
+        {ServiceList.map((service) => (
+          <Service key={service.id} image={getImage(service.image)} name={service.name} />
         ))}
       </div>
       <div className='text-xs font-semibold'>Temukan promo menarik</div>
@@ -78,8 +51,8 @@ function Home() {
         onMouseUp={onMouseUp}
         onMouseLeave={onMouseLeave}
       >
-        {banners.map((banner, i) => (
-          <Banner key={i} src={banner.src} alt={banner.alt} />
+        {BannerList.map((banner) => (
+          <Banner key={banner.id} src={banner.image} alt={banner.name} />
         ))}
       </div>
     </div>
