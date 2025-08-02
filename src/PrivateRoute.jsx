84602ed3a566a623b3c01b50d42a70218a5336fb
rrhmn <raissarahmani18@gmail.com>
2025-08-02 21:13:02 +0700

    add: private route

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -1,7 +1,10 @@
-import { Navigate, Outlet } from "react-router"
-import { useSelector } from "react-redux"
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate, Outlet } from 'react-router-dom'
 
-export const PrivateRoute = () => {
-  const user = useSelector((state) => state.auth.token)
-  return user ? <Outlet /> : <Navigate to="/login" />
+function PrivateRoute() {
+  const token = useSelector((state) => state.auth.token)
+  return token ? <Outlet /> : <Navigate to="/login" replace />
 }
+
+export default PrivateRoute
